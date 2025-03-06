e530d3979a6c8723bdc5e3d43815136e4a8691f4
Tanatos <dcb97.kts@gmail.com>
2025-03-05 18:01:50 -0600

    Commit inicial

---
+++ models/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');   
+
+
+class Server {
+
+    constructor(name, ip, port) {
+        this.app = express();
+        this.port = process.env.Port;
+
+        //midedlewares
+        this.middlewares();
+
+
+        this.routes();
+    }
+
+    middlewares() {
+
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.static('public'));
+        
+    }
+
+    routes() {
+
+        this.app.use('/api/user', require('../routes/user'));
+
+        
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Server corre en ${this.port}`)
+        })
+    }
+}
+
+
+module.exports = Server;
